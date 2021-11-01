7a9ffcca11edf6412faca14d8180e73458ab624e
Satyaki De Sarkar <satyakidesarkar2017@gmail.com>
2021-11-01 19:23:07 +0530

    ok

---
+++ main.py
@@ -0,0 +1,30 @@
+print("Hello world!");
+print(type(6));
+
+# Data types 
+x = ['satyaki','snehal'];
+y= "satyaki"
+
+print(y[1]);
+print(type(x));
+
+# Concatinate
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+# Array types
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+# Range aaray
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
